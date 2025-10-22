1b0bbc31f83de18149a0c45aa98a8e6ae06007d9
Rishita Raj <rishitaraj1855@gmail.com>
2025-10-22 12:34:28 +0530

    util code

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,29 @@
+//common in all
+
+const asyncHandler = (requestHandler) =>{
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req,res,next)).
+        catch((err) => next(err))
+    } 
+}
+
+
+export {asyncHandler}
+
+
+
+
+// xonst asyncHandler = () =>{}
+// xonst asyncHandler = (func) => () => {}
+// xonst asyncHandler = (func) => async () => {}
+
+    // const asyncHandler = (fn) => async (req, res, next) => {
+    //     try {
+    //         await fn(req,res, next)
+    //     } catch (error) {
+    //         res.status(error.code || 500).json({
+    //             success: false,
+    //             message: error.message
+    //         })
+    //     }
+    // }
